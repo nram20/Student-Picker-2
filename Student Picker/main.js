951fbbb20fe1042ef33733b3913a8650e26249b0
Nikhil Ram <nikhilram00@gmail.com>
2016-06-07 19:05:16 -0700

    Update main.js

--- Student Picker/main.js
+++ Student Picker/main.js
@@ -5,7 +5,7 @@ function init() {
 
 
 document.getElementById('createRoster').addEventListener("click", createInputs);
-
+// Takes the input for desired number of students and creates a blank "roster" of inputs
   function createInputs() {
     var numStudents = document.getElementById('studentNumber').value;
     console.log("number of students: ", numStudents)
@@ -21,7 +21,7 @@ document.getElementById('picker').addEventListener("click", pickStudents);
 
   function pickStudents() {
 
-    // remove "picked" class from all inputs to reset 
+    // first, remove "picked" class from all inputs to reset 
     var allinputs = document.getElementsByTagName('input')
       for(var i = allinputs.length - 1; i>= 0; --i) {
         allinputs[i].classList.remove('picked')
@@ -33,7 +33,7 @@ document.getElementById('picker').addEventListener("click", pickStudents);
     var random = Math.floor(Math.random()*numStudents) + 1 ;
     console.log("random student number: ", random); 
     
-    // Each Input Box Has a class of 1, 2, 3 -> numStudents, so I want to get the input box with class = random, and then add "picked" class, so that the input will be highlighted and bordered
+    
         
 
     var randomInput = allinputs[random];
